b0fa793543325171a3971e5cf290b7a42314f436
Danila <Danila@DESKTOP-44LP0PV>
2025-10-22 16:53:25 +0300

    nu tipa game DONE

---
+++ DataTypes/Program.cs
@@ -0,0 +1,90 @@
+﻿//#define DATA_TYPES
+//#define CONSTANTS
+#define TYPE_CONVERSION
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+	class Program
+	{
+		const string delimiter = "\n-------------------------------------------------------\n";
+		static void Main(string[] args)
+		{
+#if DATA_TYPES
+			Console.WriteLine($"bool занимает {sizeof(bool)} Байт памяти, класс обёртка Boolean;");
+			Console.WriteLine(bool.FalseString);
+			Console.WriteLine(bool.TrueString);
+			Console.WriteLine(typeof(bool));
+			Console.WriteLine(delimiter);
+			bool rain = true;
+			Console.WriteLine($"Дождь идёт? - {rain}");
+			///////////////////////////////////////////////////////////////////////////////////////
+
+			Console.WriteLine
+				(
+$@"Тип данных char занимает {sizeof(char)} Байт памяти, и принимает значения в диапазоне
+от {(int)char.MinValue} до {(int)char.MaxValue}. Класс-обёртка - {typeof(char)}"
+				);
+			Console.WriteLine(delimiter);
+			//@"Raw-строка"
+			Console.WriteLine(@"Raw-строка - игнорирует все специальные символы и esc-последовательности \t\n, т.е., воспринимается как есть 'as-is'");
+			//ushort, uint, ulong
+			//sbyte		byte	1 байт
+			//short		ushort	2 байта
+			//int		uint	4 байта
+			//long		ulong	8 байт
+			Console.WriteLine
+	(
+$@"byte занимает {sizeof(byte)} Байт памяти, и принимает значения в диапазоне от
+{byte.MinValue} до {byte.MaxValue}, класс-обёртка {typeof(byte)}"
+	);
+			Console.WriteLine(delimiter);
+
+			Console.WriteLine
+				(
+$@"byte занимает {sizeof(sbyte)} Байт памяти, и принимает значения в диапазоне от
+{sbyte.MinValue} до {sbyte.MaxValue}, класс-обёртка {typeof(sbyte)}"
+				);
+			Console.WriteLine(delimiter);
+
+			Console.WriteLine($@"float занимает {sizeof(float)} Байт памяти");
+
+
+			Console.WriteLine($@"decimal занимает {sizeof(decimal)} Байт памяти");
+
+#endif
+			//////////////////////////////////////////////////////////////////////////////////////
+
+
+#if CONSTANTS
+			Console.WriteLine("Hello".GetType());
+			//Console.WriteLine(typeof("Hello"));
+			Console.WriteLine(5.0.GetType());
+#endif
+
+#if TYPE_CONVERSION
+			int n = 5;
+			while (n-- > 0)
+			{
+				Console.WriteLine(n);
+			}
+			Console.WriteLine(delimiter);
+			//ushort b = (ushort) - 2;
+			//Console.WriteLine(b);
+			double a = 2.2;
+			short b = (short)a;
+			Console.WriteLine(a);
+			// C-Like notation	(type)value
+			bool rain = Convert.ToBoolean("True");
+			bool rain_1 = bool.Parse("False");
+			Console.WriteLine(rain);
+			//Console.WriteLine(rain as string);
+			Console.WriteLine(rain_1);
+#endif
+		}
+	}
+}
---
+++ Game/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Game
+{
+	class Program
+	{
+		static int PlayerX = 0;	//начальная позиция символа
+		static int PlayerY = 0;
+		static char[] Player = {'@'};	//сам 'игрок'
+		static int OurPosition = 0;		//Текущая позиция
+		static bool GameRunning = true;	//Для задачи условия цикла в мэйне
+		static int fieldWidth = 1000;	//Граница полей для ориентира
+		static int fieldHeight = 1000;
+		static void Main(string[] args)
+		{
+			while (GameRunning)	//Условия работы игры
+			{
+				Game();
+				Management();
+			}
+		}
+		static void Game()	
+		{
+			Console.SetCursorPosition(PlayerX, PlayerY);	//Для перемещения в консоли по координатам
+			Console.Write(Player[OurPosition]);
+		}
+		static void Management()
+		{
+			if (Console.KeyAvailable)
+			{
+				ConsoleKeyInfo key = Console.ReadKey(true);
+				switch (key.Key)
+				{
+					case ConsoleKey.W:
+						if (PlayerY > 1) PlayerY--;
+						Console.Clear();
+						break;
+					case ConsoleKey.S: 
+						if (PlayerY < fieldHeight - 1)  PlayerY++;
+						Console.Clear();
+						break;
+					case ConsoleKey.A: 
+						if (PlayerX > 1) PlayerX--;
+						Console.Clear();
+						break;
+					case ConsoleKey.D:
+						if (PlayerX < fieldWidth - 1) PlayerX++;
+						Console.Clear();
+						break;
+					case ConsoleKey.Escape:
+						GameRunning = false;
+						break;
+				}
+			}
+		}
+	}
+}
